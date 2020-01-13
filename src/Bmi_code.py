04229bf3eb4dfd4c56465c0149562bce5116634f
Alon Gabay <gabai.alon@gmail.com>
2020-01-13 15:45:31 +0200

    Bmi_code

---
+++ src/Bmi_code.py
@@ -0,0 +1,24 @@
+class Bmi_code:
+    
+    @staticmethod
+    def bmi_calc(weight, height):
+        if type(weight) != int and type(weight) != float:
+            return "Worng Type"
+        if height <= 0 :
+            return "Invalid height"
+        if weight <= 0:
+            return "Invalid weight"
+        return weight / pow(height,2)
+
+    @staticmethod
+    def bmi_check(bmi):
+        if type(bmi) != int and type(bmi) != float:
+            return "Worng Type"
+        if(bmi > 18.5 and bmi < 25):
+            return "A proper bmi"
+        elif (bmi > 25):
+            return "Overweight"
+        elif (bmi > 0 and bmi < 18):
+            return "Underweight"
+        elif (bmi < 0):
+            return "Invalid_Bmi"
